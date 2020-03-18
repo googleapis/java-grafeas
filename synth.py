acf2e6c78fd7047b22ca455adaa39dcda70b09a3
Jeff Ching <chingor@google.com>
2020-03-18 10:37:56 -0700

    chore: use java helper for templates

--- synth.py
+++ synth.py
@@ -88,8 +88,6 @@ for version in versions:
 
 
 java.format_code('./src')
-common_templates = gcp.CommonTemplates()
-templates = common_templates.java_library()
-s.copy(templates, excludes=[
+java.common_templates(excludes=[
     'README.md',
 ])
