2bc979afde66dce13144356ad75df3debf79152d
yoshi-automation <yoshi-automation@google.com>
2020-12-03 16:39:33 -0800

    fix: Update gapic-generator-java to 0.0.7
    Committer: @miraleung
    PiperOrigin-RevId: 345476969
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Dec 3 10:07:32 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 7be2c821dd88109038c55c89f7dd48f092eeab9d
    Source-Link: https://github.com/googleapis/googleapis/commit/7be2c821dd88109038c55c89f7dd48f092eeab9d

--- src/main/java/io/grafeas/v1/OccurrenceName.java
+++ src/main/java/io/grafeas/v1/OccurrenceName.java
@@ -91,7 +91,7 @@ public class OccurrenceName implements ResourceName {
   public static List<String> toStringList(List<OccurrenceName> values) {
     List<String> list = new ArrayList<>(values.size());
     for (OccurrenceName value : values) {
-      if (Objects.isNull(value)) {
+      if (value == null) {
         list.add("");
       } else {
         list.add(value.toString());
@@ -106,14 +106,14 @@ public class OccurrenceName implements ResourceName {
 
   @Override
   public Map<String, String> getFieldValuesMap() {
-    if (Objects.isNull(fieldValuesMap)) {
+    if (fieldValuesMap == null) {
       synchronized (this) {
-        if (Objects.isNull(fieldValuesMap)) {
+        if (fieldValuesMap == null) {
           ImmutableMap.Builder<String, String> fieldMapBuilder = ImmutableMap.builder();
-          if (!Objects.isNull(project)) {
+          if (project != null) {
             fieldMapBuilder.put("project", project);
           }
-          if (!Objects.isNull(occurrence)) {
+          if (occurrence != null) {
             fieldMapBuilder.put("occurrence", occurrence);
           }
           fieldValuesMap = fieldMapBuilder.build();
